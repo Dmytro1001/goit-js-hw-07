68774649240326d65ffb740a0ec55e40d43b82ee
Dmytro1001 <31dmitri13@gmail.com>
2022-12-11 12:40:31 +0200

    Task 1-2 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 const markup = galleryItems.reduce((acc, { preview, original, description }) => acc + `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryIt = document.querySelector('.gallery');
+const markup = galleryItems.reduce((acc, { preview, original, description }) => acc + `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`, '');
+
+galleryIt.insertAdjacentHTML('beforeend', markup);
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData:'alt', captionPosition: "bottom", captionDelay: 250});
+gallery.on('show.simplelightbox', function () {
+});
